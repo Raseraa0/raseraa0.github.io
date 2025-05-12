9688ec67eb0e29247004d7cb029882d14e0c4f43
RASERA Arthur <arthur.rasera@soprasteria.com>
2025-05-12 10:40:26 +0200

    teste priority badge

--- app/components/SocialMedia.tsx
+++ app/components/SocialMedia.tsx
@@ -11,9 +11,9 @@ function SocialMedia({ svgSrc, className = "", num = "0" }: Props) {
     <button
       className={`p-social-media-badge animate-shimmer bg-[linear-gradient(110deg,#002545,45%,#003b64,55%,#002545)] bg-[length:200%_100%]  transition-colors ${className}`}
       data-num={parseInt(num)}
-      id = {`social-media-badge-${num}`}
+      id={`social-media-badge-${num}`}
     >
-      <Image src={svgSrc} alt="" width={200} height={200} />
+      <Image src={svgSrc} alt="" width={200} height={200} priority/>
     </button>
   );
 }
